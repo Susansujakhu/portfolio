dbaba43d533af49e23d5384dedc13198dc6b3c6f
Susansujakhu <sushansujakhu14@gmail.com>
2022-09-14 09:32:06 +0545

    signals added

--- users/signals.py
+++ users/signals.py
@@ -4,6 +4,12 @@ from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = Profile(user=user)
+        profile.save()
+
 def build_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
